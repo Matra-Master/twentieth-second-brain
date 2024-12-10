1111f5a3b10bb3b4985c6479f093bdb6f71b7a43
Franco Carabajal <carabajal.francogabriel@gmail.com>
2024-12-10 16:23:17 -0300

    feat(quartz): updated title and subtitle

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    pageTitleSuffix: "",
+    pageTitle: "Fran's Obsidian things",
+    pageTitleSuffix: "My nth attempt at having a second brain mostly tidy",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
